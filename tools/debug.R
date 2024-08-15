61b55d4a450ed7ac38daa5896ea2b36f9409cbd7
Zachary Waller <zachwaller1@gmail.com>
2024-08-15 20:40:25 +0100

    Added extra debug code with better comparisons

--- tools/debug.R
+++ tools/debug.R
@@ -1,15 +1,54 @@
-devtools::clean_dll()
 devtools::load_all()
 
-#sourceCpp("src/newton.cpp")
-
+# Debug example 1 --------------------------------------------------------------
 lambda <- c(0.1, 0.1, 0.1)
-cum_lambda <- cumsum(lambda)
-l <- c(1, 1, 2)
-r <- c(2, 3, 3)
+l <- c(0, 0, 1, 2)
+r <- c(1, 3, 2, 3)
 q <- c(1, 2, 3)
+R0 <- c(4, 4, 4, 4)
 
-calc_deriv_R <- function(cum_lambda, l, r, q) {
-    .Call('src/newton.o', cum_lambda, l, r, q)
-}
-calc_deriv_R(cum_lambda, l, r, q)
+newt <- newton_algorithm(lambda, l, r, q)
+exp(-cumsum(newt$lambda_0))
+newt
+
+em <- em_algorithm(lambda, l, r, q, R0)
+cumprod(1 - em$h)
+em
+
+comb <- combined_algorithm(lambda, l, r, q, R0)
+comb
+exp(-cumsum(comb$lambda_0))
+
+which.max(c(comb$like, em$like, newt$like))
+which.min(c(comb$it, em$it, newt$it))
+
+comb$it
+em$it
+newt$it
+
+# Debug example 2 --------------------------------------------------------------
+l <- c(0, 0, 1, 2)
+r <- c(1, 2, 3, 3)
+q <- c(1, 2, 3)
+lambda <- c(0.1, 0.1, 0.1)
+R0 <- c(4, 4, 4, 4)
+
+newt <- newton_algorithm(lambda, l, r, q)
+exp(-cumsum(newt$lambda_0))
+newt
+
+em <- em_algorithm(lambda, l, r, q, R0)
+cumprod(1 - em$h)
+em
+
+comb <- combined_algorithm(lambda, l, r, q, R0)
+comb
+exp(-cumsum(comb$lambda_0))
+
+which.max(c(comb$like, em$like, newt$like))
+which.min(c(comb$it, em$it, newt$it))
+
+comb$it
+em$it
+newt$it
+
