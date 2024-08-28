7dcf9294da4ec7382bbfd636563ffa3eb2300da4
Zachary Waller <zachwaller1@gmail.com>
2024-08-28 14:49:12 +0100

    Updated R wrapper function ltic_np

--- R/ltic_np.R
+++ R/ltic_np.R
@@ -18,7 +18,7 @@
 ltic_np <- function(left, right, trunc = NULL, tol = 1e-7, init = NULL,
                               max_iterations = 1e5, remove_rcens = TRUE,
                               open_L = TRUE, open_T = FALSE, constr = NULL,
-                              method = c("comb", "em", "newton")) {
+                              method = c("comb", "em", "newton", "both")) {
 
 
   method <- match.arg(method)
@@ -39,6 +39,15 @@ ltic_np <- function(left, right, trunc = NULL, tol = 1e-7, init = NULL,
 
   alpha <- indicator_matrix(intervals$II, intervals$Oi)
   beta <- indicator_matrix(intervals$II, intervals$Ti)
+
+  gamma_int <- data.frame(
+      left = 0,
+      right = intervals$Oi$left,
+      open_left = FALSE,
+      open_right = !open_L
+    )
+  gamma <- indicator_matrix(intervals$II, gamma_int)
+  deriv_1_0 <- colSums(gamma)
 
   # Left truncation ------------------------------------------------------------
   # number of participants who have entered. One for each inner interval
@@ -69,29 +78,40 @@ ltic_np <- function(left, right, trunc = NULL, tol = 1e-7, init = NULL,
   }
 
   # Initialise values ----------------------------------------------------------
-  if (is.null(init)) {
-    # good first guess for events
-    n_events <- apply(event / apply(event, 1, sum), 2, sum)
-    n_cum <- cumsum(n_events)
-    y_risk <- y_0 - c(0, n_cum[-length(n_cum)])
-    lambda <- n_events / y_risk
-    lambda[y_risk == 0] <- 1
-  }
+  # if (is.null(init)) {
+  #   # good first guess for events
+  #   n_events <- apply(event / apply(event, 1, sum), 2, sum)
+  #   n_cum <- cumsum(n_events)
+  #   y_risk <- y_0 - c(0, n_cum[-length(n_cum)])
+  #   lambda <- n_events / y_risk
+  #   lambda[y_risk == 0] <- 1
+  # }
 
   l <- apply(event, 1, function(x) min(which(x == 1)) - 1)
   r <- apply(event, 1, function(x) max(which(x == 1)))
   t <- apply(beta, 1, function(x) min(which(x == 1)) - 1)
 
+  lambda <- rep(1 / ncol(alpha), ncol(alpha))
+  lambda[is.nan(lambda)] <- 9999
+
   cat("\n Starting algorithm...")
   # Call maximisation algorithm ------------------------------------------------
   if (method == "newton") {
-    res <- newton_algorithm(lambda, l, r)
+    res <- newton_algorithm(lambda, l, r, deriv_1_0)
   } else if (method == "em") {
     res <- em_algorithm(lambda, l, r, y_0)
   } else if (method == "comb") {
-    res <- combined_algorithm(lambda, l, r, y_0)
+    res <- combined_algorithm(lambda, l, r, deriv_1_0, y_0)
+  } else if (method == "both") {
+    res <- ltic_r(lambda, l, r, t, y_0)
   }
 
-  res
-
+  list(
+    res = res,
+    l = l,
+    r = r,
+    t = t,
+    deriv_1_0 = deriv_1_0,
+    y_0 = y_0
+  )
 }
