fb82c841cd53f1bc1d7f3390c9f36e605a0ca554
Zachary Waller <zachwaller1@gmail.com>
2024-10-10 20:32:07 +0100

    Added Turnbull and Yu with ICM

---
+++ src/ltic_yu.cpp
@@ -0,0 +1,237 @@
+#include <Rcpp.h>
+#include <iostream>
+#include "ltic_yu.h"
+#include "monotone.h"
+using namespace Rcpp;
+
+
+// [[Rcpp::export]]
+List ltic_yu_r(NumericVector lambda, IntegerVector l, IntegerVector r, 
+            IntegerVector t, double toler, int max_it) {
+
+    ltic_yu ltic_ob(lambda, l, r, t, toler, max_it);
+
+    ltic_ob.run();
+
+    List out;
+    out["llike"] = ltic_ob.llike;
+    out["it"] = ltic_ob.it;
+    out["lambda"] = ltic_ob.cum_lambda;
+    out["tol"] = ltic_ob.tol;
+    //out["conv"] = ltic_ob.calc_conv();
+
+    return out;
+
+}
+
+// outer loop
+void ltic_yu::run() {
+  double old_like = R_NegInf;
+  while (it < maxit && !conv) {
+    em_algo();
+    convert_to_haz();
+    newton_algo();
+    llike = calc_like();
+    conv = llike - old_like < tol && llike - old_like > -tol;
+    old_like = llike;
+    it++;
+    convert_to_surv();
+  }
+}
+
+// calculate likelihood
+double ltic_yu::calc_like() {
+  double like = 0;
+  for (int i = 0; i < n_obs; i++) {
+      like += log( exp(-cum_lambda[left[i]] + cum_lambda[trun[i]]) - 
+        exp(-cum_lambda[right[i]] + cum_lambda[trun[i]]));
+  }
+
+  return like;
+}
+
+// EM algorithm
+void ltic_yu::em_algo() {
+
+    int it_em = 0;
+    double total_weight = 0;
+    while (it_em < 10) {
+
+      calc_weight_sums();
+
+      // calculate new s values
+      for (int j = 0; j < n_int; j++) {
+          s_1[j] = s_0[j] * w_sum[j] / n;
+          // if (s_1[j] < 0) {
+          //   std::cout << "whaet" << std::endl;
+          // }
+      }
+      
+      // reset for next iteration
+      for (int j = 0; j < n_int; j++) {
+          s_0[j] = s_1[j];
+          surv[j + 1] = surv[j] - s_1[j];
+          if (surv[j + 1] < 0.) {
+            surv[j + 1] = 0.0;
+            s_0[j] = surv[j];
+          }
+          w_sum[j] = 0;
+      }
+      it_em++;
+    }
+}
+
+void ltic_yu::calc_weight_sums() {
+
+  int l_size, r_size, t_size;
+
+  // first interval
+  int size_0 = lr_inv[0].in.size();
+  int curr;
+  w_sum[0] = n_obs;
+  for (int i = 0; i < size_0; i++) {
+    curr = lr_inv[0].in[i];
+    w_sum[0] = w_sum[0] + (1. / (surv[left[curr]] - surv[right[curr]]));
+  }
+
+  size_0 = lr_inv[0].trun_time.size();
+  for (int i = 0; i < size_0; i++) {
+    w_sum[0] = w_sum[0] - 1.;
+  }
+
+  // loop through the rest
+  for (int j = 1; j < n_int; j++) {
+
+    l_size = lr_inv[j].in.size();
+    r_size = lr_inv[j].out.size();
+    t_size = lr_inv[j].trun_time.size();
+    w_sum[j] = w_sum[j - 1];
+
+    // in
+    for (int i = 0; i < l_size; i++) {
+      curr = lr_inv[j].in[i];
+      w_sum[j] = w_sum[j] + (1. / (surv[left[curr]] - surv[right[curr]]));
+    }
+
+    // out
+    for (int i = 0; i < r_size; i++) {
+      curr = lr_inv[j].out[i];
+      w_sum[j] = w_sum[j] - (1. / (surv[left[curr]] - surv[right[curr]]));
+    }
+
+    // trunc
+    for (int i = 0; i < t_size; i++) {
+      curr = lr_inv[j].trun_time[i];
+      w_sum[j] = w_sum[j] - 1. / surv[trun[curr]];
+    }
+  }
+
+}
+
+
+void ltic_yu::convert_to_haz() {
+  for (int j = 1; j < n_int; j++) {
+    if (surv[j] >= 0) {
+      cum_lambda[j] = -log(surv[j]);
+    } else {
+      cum_lambda[j] = R_PosInf;
+    }
+  }
+}
+
+void ltic_yu::convert_to_surv() {
+  for (int j = 0; j < n_int; j++) {
+    surv[j + 1] = exp(-cum_lambda[j + 1]);
+    s_0[j] = surv[j] - surv[j + 1];
+  }
+}
+
+
+void ltic_yu::newton_algo() {
+
+    calc_derivs();
+    half_steps();
+
+    // reset values
+    for (int j = 0; j < n_int; j++) {
+        // reset for next iteration
+        deriv_1[j] = 0;
+        deriv_2[j] = 0;
+    }
+}
+
+void ltic_yu::calc_derivs() {
+    double surv_diff;
+    double derv_right, derv_left;
+
+    for (int i = 0; i < n_obs; i++) {
+
+      derv_left = exp(-cum_lambda[left[i]] + cum_lambda[trun[i]]);
+      derv_right = exp(-cum_lambda[right[i]] + cum_lambda[trun[i]]);
+      surv_diff = derv_left - derv_right;
+
+      deriv_1[trun[i]] += 1.;
+      deriv_1[left[i]] += -derv_left / surv_diff;
+      deriv_1[right[i]] += derv_right / surv_diff;
+
+      deriv_2[left[i]] += derv_left / surv_diff - derv_left * derv_left / (surv_diff * surv_diff);
+      deriv_2[right[i]] += -derv_right / surv_diff - derv_right * derv_right / (surv_diff * surv_diff);
+    }
+}
+
+// half stepping
+void ltic_yu::half_steps() {
+    int tries = 0;
+    bool inc_lik = false;
+    double temp_lk = calc_like();
+    double new_lk;
+    double alpha = -1;
+    
+    int n_weight = n_int - 1;
+    double w[n_weight];
+    double y[n_weight];
+    double diff[n_weight];
+
+    for (int j = 0; j < n_weight; j++) {
+      if (deriv_2[j + 1] >= 0.) deriv_2[j + 1] = -1e-9;
+      //   y[j] = cum_lambda[j + 1];
+      //   w[j] = 1e9;
+      // } else {
+        y[j] = -deriv_1[j + 1] / deriv_2[j + 1] + cum_lambda[j + 1];
+        w[j] = deriv_2[j + 1] / 2;
+      //}
+      //if (y[j] < 0) y[j] = 0;
+    }
+
+    /* PAVA algorithm */
+    monotoneC(&n_weight, y, w);
+
+    for (int j = 0; j < n_weight; j++) {
+      if (y[j] < 0) y[j] = 0.;
+    }
+
+    for(int j = 0; j < n_weight; j++){
+        diff[j] = y[j] - cum_lambda[j + 1];
+    }
+
+    for (int j = 0; j < n_weight; j++) {
+      cum_lambda[j + 1] = cum_lambda[j + 1] + diff[j];
+    }
+
+    /* Half stepping */
+    new_lk = calc_like();
+    inc_lik = new_lk >= temp_lk;
+
+    while (tries < 15 && !inc_lik) {
+      alpha *= 0.5;
+
+      for (int j = 0; j < n_weight; j++) {
+        cum_lambda[j] = cum_lambda[j] + alpha * diff[j];
+      }
+
+      new_lk = calc_like();
+
+      tries++;
+      inc_lik = new_lk >= temp_lk;
+    }
+}
---
+++ src/ltic_yu.h
@@ -0,0 +1,126 @@
+class ltic_yu{
+  public:
+    int n_int;
+    int n_obs;
+    double n;
+
+
+    std::vector<int> left;
+    std::vector<int> right;
+    std::vector<int> trun;
+    std::vector<double> s_0;
+    std::vector<double> s_1;
+
+    std::vector<double> deriv_1;
+    std::vector<double> deriv_2;
+    std::vector<double> cum_lambda;
+    std::vector<double> surv;
+    std::vector<double> n_trans, cum_n_trans, h, w_sum;
+
+    class invert_data{
+      public:
+        std::vector<int> in;
+        std::vector<int> out;
+        std::vector<int> trun_time;
+    };
+
+    std::vector<invert_data> lr_inv;
+
+    double tol;
+    int maxit;
+    int it = 0;
+    bool conv = false;
+    double llike = R_NegInf;
+
+    double calc_like();
+    void em_algo();
+    void calc_weight_sums();
+    void convert_to_haz();
+    void convert_to_surv();
+    void newton_algo();
+    void calc_derivs();
+    void half_steps();
+    void run();
+
+
+    // constructor
+    ltic_yu(Rcpp::NumericVector lambda, Rcpp::IntegerVector l, 
+         Rcpp::IntegerVector r, Rcpp::IntegerVector t,
+         double toler, int max_it) {
+
+      tol = toler;
+      maxit = max_it;
+      n_int = lambda.length();
+      n_obs = l.length();
+      n = static_cast<double>(n_obs);
+
+      left = Rcpp::as< std::vector<int> >(l);
+      right = Rcpp::as< std::vector<int> >(r);
+      trun = Rcpp::as<std::vector<int> >(t);
+      h = Rcpp::as<std::vector<double> > (lambda);
+      s_0 = Rcpp::as< std::vector<double> >(lambda);
+      s_1 = s_0;
+
+      deriv_1.resize(n_int);
+      deriv_2.resize(n_int);
+      cum_lambda.resize(n_int + 1);
+      surv.resize(n_int + 1, 1);
+      n_trans.resize(n_int);
+      cum_n_trans.resize(n_int + 1);
+      w_sum.resize(n_int);
+
+      /* Initiate Cumulative Hazards */
+      // for (int j = 1; j < n_int + 1; j++){
+      //     cum_lambda[j] = cum_lambda[j - 1] + lambda[j - 1];
+      // }
+      cum_lambda[0] = 0;
+      cum_lambda[n_int] = R_PosInf;
+
+      // initiate surv
+      surv[0] = 1;
+      for (int j = 0; j < n_int; j++){
+          surv[j + 1] = surv[j] - s_0[j];
+      }
+
+      /* Invert Data */
+      lr_inv.resize(n_int + 1);
+      std::vector<int> left_sizes(n_int + 1);
+      std::vector<int> right_sizes(n_int + 1);
+      std::vector<int> trun_sizes(n_int + 1);
+
+      // find sizes
+      for (int i = 0; i < n_obs; i++) {
+        left_sizes[left[i]]++;
+        right_sizes[right[i]]++;
+        trun_sizes[trun[i]]++;
+      }
+
+      // make data_inv correct size
+      for (int j = 0; j < n_int + 1; j++) {
+        lr_inv[j].in.resize(left_sizes[j]);
+        lr_inv[j].out.resize(right_sizes[j]);
+        lr_inv[j].trun_time.resize(trun_sizes[j]);
+      }
+
+      int curr_l, curr_r, curr_t;
+      int curr_j_l, curr_j_r, curr_j_t;
+      // transpose data
+      for (int i = 0; i < n_obs; i++) {
+        curr_l = left[i];
+        curr_r = right[i];
+        curr_t = trun[i];
+        curr_j_l = left_sizes[curr_l] - 1;
+        curr_j_r = right_sizes[curr_r] - 1;
+        curr_j_t = trun_sizes[curr_t] - 1;
+        lr_inv[curr_l].in[curr_j_l] = i;
+        lr_inv[curr_r].out[curr_j_r] = i;
+        lr_inv[curr_t].trun_time[curr_j_t] = i;
+
+        left_sizes[curr_l]--;
+        right_sizes[curr_r]--;
+        trun_sizes[curr_t]--;
+      }
+
+    };
+
+};
